982d959e82145ff2ec64113f4cd09004982b0068
aleks070582 <aleks.ryzhov82@gmail.com>
2025-02-16 19:53:05 +0400

    переработал полностью

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -9,54 +9,55 @@
 #include<utility>
 #include"geo.h"
 #include<unordered_set>
-
+#include<optional>
 namespace project
 {
-	using namespace geo;
+	
 	namespace transport_catalogue
 	{
-		struct StopInfo
-		{
-			bool exist;
-			std::vector <std::string> buses;
-
-		};
-
-		struct Stop
-		{
-			std::string name;
-			Coordinates coord;
-		};
-
-		struct Bus
-		{
-			std::string name;
-		};
-
+		struct Bus;
+		struct Stop;
+		
+		
 		struct BusInfo
 		{
-			std::string name = "";
-			int number_of_stops = 0;
-			int number_of_u_stops = 0;// кол-во уникальных остановок;
-			double route_lenght = 0;
+			unsigned int stops;
+			unsigned int u_stops;
+			double lenght;
 		};
+		struct Stop
+
+		{
+			Stop(const std::string&name_,const geo::Coordinates coord_) :name(name_),
+				coord(coord_),buses{}
+			{
+			};
+			std::string name;
+			geo::Coordinates coord;
+			std::vector<Bus*> buses;
+		};
+		struct Bus
+		{
+			Bus(const std::string& _name) :name(_name), stops{}
+			{
+			};
+			std::string name;
+			std::vector<Stop*> stops;
+		}; 
+		
 		class TransportCatalogue
 		{
 		public:
-			StopInfo get_stop_info(std::string& name) const;
-			BusInfo get_bus_info(const std::string& name) const;
-			void add_bus(std::string& name);
-			void add_bus_route(std::string& name, std::vector<std::string_view>&& route);
-			void add_stop(std::string& name, Coordinates coord);
+			const std::optional<std::vector<std::string_view>> get_stop_info(const std::string& name) const;
+		    std::optional<BusInfo> get_bus_info(const std::string& name) const;
+			void add_stop(const std::string& name, const geo::Coordinates coord);
+			void add_bus(const std::string& name, const std::vector<std::string_view>&& route);
 		private:
-			double calculate_route(std::string* p_name) const;
-			int get_unique_stops(std::string* p_name) const;
-			Bus* find_bus(std::string& name);
-			Stop* find_stop(std::string& name);
+			double calculate_path(const Bus* p_bus) const;
 			std::deque<Stop> stops;
 			std::deque<Bus> buses;
-			std::unordered_map<std::string*, std::vector<Bus*>> stops_to_bus;
-			std::unordered_map<std::string*, std::vector<Stop*>> bus_to_stops;
+			std::unordered_map<std::string, Bus*> p_to_bus;
+			std::unordered_map<std::string, Stop*> p_to_stop;
 		};
 	}
 }
