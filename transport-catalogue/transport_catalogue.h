2254a3930e18cc88f74715aec0b4906844f409e7
aleks070582 <aleks.ryzhov82@gmail.com>
2025-02-16 13:43:20 +0400

    Update transport_catalogue.h

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -1,1 +1,62 @@
-// место для вашего кода
+#pragma once
+#include<deque>
+#include<string>
+#include<vector>
+#include<unordered_map>
+#include<iterator>
+#include<functional>
+#include<algorithm>
+#include<utility>
+#include"geo.h"
+#include<unordered_set>
+
+namespace project
+{
+	using namespace geo;
+	namespace transport_catalogue
+	{
+		struct StopInfo
+		{
+			bool exist;
+			std::vector <std::string> buses;
+
+		};
+
+		struct Stop
+		{
+			std::string name;
+			Coordinates coord;
+		};
+
+		struct Bus
+		{
+			std::string name;
+		};
+
+		struct BusInfo
+		{
+			std::string name = "";
+			int number_of_stops = 0;
+			int number_of_u_stops = 0;// кол-во уникальных остановок;
+			double route_lenght = 0;
+		};
+		class TransportCatalogue
+		{
+		public:
+			StopInfo get_stop_info(std::string& name) const;
+			BusInfo get_bus_info(const std::string& name) const;
+			void add_bus(std::string& name);
+			void add_bus_route(std::string& name, std::vector<std::string_view>&& route);
+			void add_stop(std::string& name, Coordinates coord);
+		private:
+			double calculate_route(std::string* p_name) const;
+			int get_unique_stops(std::string* p_name) const;
+			Bus* find_bus(std::string& name);
+			Stop* find_stop(std::string& name);
+			std::deque<Stop> stops;
+			std::deque<Bus> buses;
+			std::unordered_map<std::string*, std::vector<Bus*>> stops_to_bus;
+			std::unordered_map<std::string*, std::vector<Stop*>> bus_to_stops;
+		};
+	}
+}
