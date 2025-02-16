f22643c683ce6df595b155bc2ce645c42c464b8a
aleks070582 <aleks.ryzhov82@gmail.com>
2025-02-16 19:48:43 +0400

    поправил

--- transport-catalogue/geo.h
+++ transport-catalogue/geo.h
@@ -21,10 +21,13 @@ namespace project
             if (from == to) {
                 return 0;
             }
+            
+            
+            static const int r_earth = 6371000;
             static const double dr = 3.1415926535 / 180.;
             return acos(sin(from.lat * dr) * sin(to.lat * dr)
                 + cos(from.lat * dr) * cos(to.lat * dr) * cos(abs(from.lng - to.lng) * dr))
-                * 6371000;
+                * r_earth;
         }
     }
 }
