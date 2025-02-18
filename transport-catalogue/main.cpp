6877a838eb1f19ae1472c92cfaeeb508b0c27b78
aleks070582 <aleks.ryzhov82@gmail.com>
2025-02-18 18:58:28 +0400

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -10,26 +10,10 @@ using namespace std;
 
 
 int main() {
-    project::transport_catalogue:: TransportCatalogue catalogue;
+    project::transport_catalogue::TransportCatalogue catalogue;
+    fstream temp("tsB_case1_input.txt");
+    project::input_reader::ReadInput(catalogue, temp);
 
-    int base_request_count;
-    cin >> base_request_count >> ws;
-
-    {
-        project::input_reader::InputReader reader;
-        for (int i = 0; i < base_request_count; ++i) {
-            string line;
-            getline(cin, line);
-            reader.ParseLine(line);
-        }
-        reader.ApplyCommands(catalogue);
-    }
-
-    int stat_request_count;
-    cin >> stat_request_count >> ws;
-    for (int i = 0; i < stat_request_count; ++i) {
-        string line;
-        getline(cin, line);
-       project::stat_reader::ParseAndPrintStat(catalogue, line, cout);
-    }
+    project::stat_reader::ReadInput(temp, cout, catalogue);
+    
 }
