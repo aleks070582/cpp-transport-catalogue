c40718a563e16ee07acf4b9138ddf7db536bfadd
aleks070582 <aleks.ryzhov82@gmail.com>
2025-02-18 18:59:35 +0400

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -4,11 +4,18 @@
 #include <string_view>
 #include<utility>
 #include "transport_catalogue.h"
+#include<set>
+#include<sstream>
+
 namespace project {
     namespace stat_reader
     {
+        void ReadInput(std::istream& in, std::ostream &out, transport_catalogue::TransportCatalogue&
+            transport_catalogue);
 
         void ParseAndPrintStat(const transport_catalogue::TransportCatalogue& transport_catalogue, std::string_view request,
             std::ostream& output);
+
+        
     }
 }
