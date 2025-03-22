ecd6710e6d39277b414fdd0a6ba6375bc28ce9cb
aleks070582 <aleks.ryzhov82@gmail.com>
2025-03-22 14:47:13 +0400

    Add files via upload
    ввел класс JsonReader

--- json_reader.h
+++ json_reader.h
@@ -22,7 +22,7 @@ struct StatRequest {
 using StopInfo = const std::set<const transport_catalogue::Bus*,transport_catalogue::BusPointerComparator>*;
 json::Node SvgToJson(const json::Document& document, const transport_catalogue::TransportCatalogue& catalog,int id);
 svg::Color ParseColorFromJson(const json::Node& node);
-RenderSettings ParsingSetting(const json::Document& document);
+RenderSettings ParsingSettings(const json::Document& document);
 json::Node BusInfoToJson(const std::optional<transport_catalogue::BusInfo>& bus_info, int id);
 json::Node StopInfoToJson(const std::optional< StopInfo>& stop, int id);
 std::vector<StatRequest> ParseStatJsonDocument(const json::Document& document);
